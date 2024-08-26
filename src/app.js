7b8ec81cacf23ad5c85202cf96711dd9d51d33ab
Davit Mkhitaryan <dmkhitar@c2r9s3.42yerevan.am>
2024-08-26 23:02:47 +0400

    mailSender doesn't work

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ const run = require('./connect')
 const path = require('path')
 const express = require('express')
 const PORT = 3000
+const dotenv = require('dotenv');
 const app = express()
+const nodemailer = require('nodemailer')
+const jwt = require('jsonwebtoken')
+dotenv.config()
+const emailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.OWNEREMAIL,
+    pass: process.env.OWNERPASS
+  }
+})
 
 /* Run connection to db */
 run().catch(console.dir)
@@ -10,9 +21,21 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: true}))
 
 app.post('/register', async (req, res)=>{
-  const email = await req.body
-  console.log(email)
-  res.end()
+  const user = await req.body
+  const emailToken = jwt.sign({
+    email: user.email
+  }, 'your_secret', {expiresIn: '1h'});
+  const verificationUrl = `http://yourdomain.com/verify-email?token=${emailToken}`;
+  console.log(process.env.OWNEREMAIL)
+  console.log(emailTransporter)
+  const mailOptions = {
+    from: process.env.OWNEREMAIL,
+    to: user.email,
+    subject: 'Verify Your Email',
+    html: `Please click the following link to verify your email: <a href="${verificationUrl}">${verificationUrl}</a>`
+  }
+  await emailTransporter.sendMail(mailOptions)
+  res.send('Registration successful, please verify your email.')
 })
 
 // starts a simple http server locally on port 3000
