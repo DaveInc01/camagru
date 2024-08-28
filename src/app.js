8f97d9ca214de19390d0af40de5c0325851a545e
Davit Mkhitaryan <dmkhitar@c2r9s3.42yerevan.am>
2024-08-28 22:23:55 +0400

    ban chem arel

--- src/app.js
+++ src/app.js
@@ -24,18 +24,33 @@ app.post('/register', async (req, res)=>{
   const user = await req.body
   const emailToken = jwt.sign({
     email: user.email
-  }, 'your_secret', {expiresIn: '1h'});
-  const verificationUrl = `http://yourdomain.com/verify-email?token=${emailToken}`;
-  console.log(process.env.OWNEREMAIL)
-  console.log(emailTransporter)
+  }, process.env.JWT_SECRET, {expiresIn: '1h'});
+  const verificationUrl = `http://localhost:${PORT}/verify-email/${emailToken}`;
   const mailOptions = {
     from: process.env.OWNEREMAIL,
     to: user.email,
     subject: 'Verify Your Email',
     html: `Please click the following link to verify your email: <a href="${verificationUrl}">${verificationUrl}</a>`
   }
-  await emailTransporter.sendMail(mailOptions)
+  console.log(user.email)
+  try{
+    await emailTransporter.sendMail(mailOptions)
+  }
+  catch(err){
+    console.log(err)
+  }
   res.send('Registration successful, please verify your email.')
+})
+
+app.get('/verify-email/:token', (req,res)=>{
+  const {token} = req.params
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
+    if(err){
+      console.log(err)
+      res.send(`It's some error ocured, try to reregiter in camagru.com`)
+    }
+    res.send()
+  })
 })
 
 // starts a simple http server locally on port 3000
