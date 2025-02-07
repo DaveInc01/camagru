c60219be245d620f76e3a268bdf83f2386eb2b83
Davit Mkhitaryan <dmkhitar@c1r4s2.42yerevan.am>
2025-02-07 23:02:02 +0400

    change ui

--- src/views/scripts/index.js
+++ src/views/scripts/index.js
@@ -1,53 +1,48 @@
 const docWidth = document.body.clientWidth;
-const cameraWidth = docWidth > 1280 ? Math.trunc(docWidth / 2.2) : Math.trunc(docWidth / 1.5)
+const cameraWidth = docWidth > 1280 ? docWidth / 2.2 : docWidth / 1.5
 const video_block = document.getElementById("video-block")
+const camera_img = document.getElementById("camera-png")
+const camera_files = document.getElementById("camera-files")
 video_block.style.width = JSON.stringify(cameraWidth) + 'px'
 console.log(JSON.stringify(cameraWidth) + 'px')
 const video = document.getElementById('video')
 const canvas = document.getElementById('canvas')
 const snap = document.getElementById('snap')
 const errorMsgElement = document.getElementById('ErrorMsg')
-const camera_png = document.getElementById("camera-png")
-const camera_files = document.getElementsByClassName("camera-files")[0]
-const splide = document.getElementsByClassName("splide")[0]
-function showStickers(){
-    document.addEventListener( 'DOMContentLoaded', function() {
-        let splide = new Splide('.splide', {
-            type   : 'loop',
-            perPage: 3,
-            perMove: 1,
-        });
-        splide.mount()
-    })
-}
- 
 const constraints = {
     audio: false,
     video : {
         width: cameraWidth, height: 500
     },
 }
-camera_png.addEventListener("click", function(){
+camera_img.addEventListener("click", async()=>{
     async function init(){
-    try{
-        const stream = await navigator.mediaDevices.getUserMedia(constraints)
-        handleSuccess(stream)
+        try{
+            const stream = await navigator.mediaDevices.getUserMedia(constraints)
+            handleSuccess(stream)
+        }
+        catch(err){
+            errorMsgElement.innerHTML = `navigator.mediaDevices.getUserMedia: ${err.toString()}`
+        }
     }
-    catch(err){
-        errorMsgElement.innerHTML = `navigator.mediaDevices.getUserMedia: ${err.toString()}`
+    function handleSuccess(stream){
+        console.log(stream)
+        window.stream = stream
+        video.srcObject = stream
+        camera_files.style.display = 'none' 
     }
-}
-function handleSuccess(stream){
-    console.log(stream)
-    window.stream = stream
-    video.srcObject = stream
-    camera_files.style.display = 'none'
-    showStickers()
-}
-init()
+    init()
+})
+
+// const mike_img = document.createElement('img')
+// mike_img.src = './img/mike.jpeg' 
+// video_block.style.cssText = `
+//     background: url(./img/mike.jpeg);
+//     background-size: contain;
+//     background-repeat: repeat;`
 
 var context = canvas.getContext('2d')
+console.log(context)
 snap.addEventListener("click", function(){
-    context.drawImage(video, 0, 0, 640, 480)
-})
+    video.drawImage(video, 0, 0, 640, 480)
 })
